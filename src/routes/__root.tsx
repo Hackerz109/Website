e3912901f88657fffe722614455efeaaee615e22
Marsha <87740278+Hackerz109@users.noreply.github.com>
2026-07-28 07:59:38 +0530

    Add files via upload

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -129,7 +129,7 @@ function RootShell({ children }: { children: ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body className="overflow-x-hidden">
+      <body>
         {children}
         <Scripts />
       </body>
