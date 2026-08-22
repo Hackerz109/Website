332c150da00f2416016785682067c312e82a0b09
Marsha <87740278+Hackerz109@users.noreply.github.com>
2026-08-22 11:20:54 +0530

    Add files via upload

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -176,8 +176,8 @@ function Index() {
           </div>
         ) : (
           <div className="grid grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-3 lg:grid-cols-4">
-            {products.map((p) => (
-              <ProductCard key={p.id} product={p} />
+            {products.map((p, i) => (
+              <ProductCard key={p.id} product={p} loading={i < 4 ? "eager" : "lazy"} />
             ))}
           </div>
         )}
